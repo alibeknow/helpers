ef8dcaa2fd9e2f13314b70a27db5b5f4780f0639
daniluk4000 <daniluk4000@yandex.ru>
2020-11-11 16:57:31 +0300

    Add code model

---
+++ src/models/misc/code.model.ts
@@ -0,0 +1,20 @@
+import { AllowNull, AutoIncrement, Column, DataType, Default, Model, PrimaryKey, Table } from 'sequelize-typescript'
+
+@Table({
+  tableName: 'codes',
+})
+export class SPCode extends Model<SPCode> {
+  @AutoIncrement
+  @PrimaryKey
+  @Column(DataType.INTEGER)
+  id: number
+
+  @AllowNull(false)
+  @Column(DataType.TEXT)
+  code: string
+
+  @AllowNull(false)
+  @Default('UNUSED')
+  @Column(DataType.TEXT)
+  codeStatus: string
+}
