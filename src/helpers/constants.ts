18bae6f2b05f73fe476e7672d0653897b272a06a
daniluk4000 <daniluk4000@yandex.ru>
2020-11-14 22:45:27 +0300

    Update dates

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,2 +1,2 @@
-export const SPStartDate = new Date(2020, 10, 9, 9);
-export const SPEndDate = new Date(2020, 10, 15, 21);
+export const SPStartDate = new Date(2020, 10, 16, 16);
+export const SPEndDate = new Date(2020, 10, 22, 17);
